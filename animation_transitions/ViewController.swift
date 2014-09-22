1d507ce75d0c7361300f1634a2f3a3af12986576
Jerry Huang <jerry.huang@livebricks.com>
2014-09-22 17:39:56 +0800

    Transition Test

--- animation_transitions/ViewController.swift
+++ animation_transitions/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let transitionManager = TransitionManager()
+    var useAnimatedTransition : Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func pushToNewViewController(sender: UIButton) {
+        var testViewController = TestViewController()
+//        testViewController.storyboard?.instantiateViewControllerWithIdentifier("HomeController")
+//        self.navigationController?.pushViewController(testViewController, animated: true)
+        
+        self.presentViewController(testViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func getSwitchedValue(sender: UISwitch) {
+        useAnimatedTransition = sender.on
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func leave(sender: UIStoryboardSegue){
+        println("leave")
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if useAnimatedTransition {
+            // this gets a reference to the screen that we're about to transition to
+            let toViewController = segue.destinationViewController as UIViewController
+            
+            // instead of using the default transition animation, we'll ask
+            // the segue to use our custom TransitionManager object to manage the transition animation
+            toViewController.transitioningDelegate = self.transitionManager
+        }
+    }
 
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return self.presentingViewController == nil ? UIStatusBarStyle.Default : UIStatusBarStyle.LightContent
+    }
 
 }
 
